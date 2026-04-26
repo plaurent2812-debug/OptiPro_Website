8e6f19dd4ec1e901468306d62667e659d6a753e4
Pierre Laurent <pl2878@proton.me>
2026-04-26 23:01:36 +0200

    fix: sitemap priorities + robots.txt avec bots IA explicites

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,11 +2,17 @@ import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/dashboard', '/admin'],
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/admin'],
+      },
+      { userAgent: 'GPTBot', allow: '/' },
+      { userAgent: 'ClaudeBot', allow: '/' },
+      { userAgent: 'PerplexityBot', allow: '/' },
+      { userAgent: 'Google-Extended', allow: '/' },
+    ],
     sitemap: 'https://www.opti-pro.fr/sitemap.xml',
   };
 }
